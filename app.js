e9813b71acaf0594665b4c882f499f70113a7164
MattiaGalasso <galasso.mattia94@gmail.com>
2026-03-16 17:36:09 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+const appURL = `http://localhost:${port}`;
+
+// IMPORTS
+
+// MIDDLEWARES
+
+// ROUTERS
+
+// ERROR HANDLERS
+
+// LISTENER
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
